0f52152cf03dc50fa5886aaa6456a61b92d22995
Theekshana Yugan <offmailacc2022@gmail.com>
2024-06-12 12:40:06 +0530

    adding services

---
+++ IGSR/service3.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Licenses for Timber transportation</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <?php
+  include_once 'header.php';
+  ?>
+
+  <div class="wrapper">
+    <div class="section">
+      <h1>Licenses for Timber transportation</h1>
+      <div class="sev-head"><p>The following steps should be followed to obtain a Timber Transport Permit.</p></div>
+
+      <div class="sev-body">
+      <p>The following documents must be submitted to obtain a timber transport permit</p>
+
+      <ul>
+        <li>A letter of request for a transport permit prepared to the Divisional Secretary.
+        </li>
+        <li>Completed application form.</li>
+        <li>List of Woods</li>
+        <li>Also certified copy of allotment of land acquired.</li>
+        <li>Logging Permit.</li>
+        <li>If the eviction order has been obtained from another institution, the same order and the recommendation of that institution.</li>
+        <li>The wood was obtained from an auction, the receipt said.</li>
+        <li>Village Officer certified cash receipt if timber purchased.</li>
+        <li>In case of donation of wood, the letter and village official certificate.</li>
+      </ul>
+      <p>Submit the above documents to the local concerned officer.</p>
+      <p>The applicable fees for a Timber Transport Permit must be paid to the Divisional Secretariat.</p>
+      <p>Recommendations will be made to the site forest office in case of limited timber transport.</p>
+      <p>If the timber specified in the relevant permit is not transported by the due date (for any reason) then the government fee shall be paid again for revising the permit.</p>
+
+      <div class="sev-head"><p>The Divisional Secretary will grant transportation approval on the recommendation of the Site Forest Officer.</p></div>
+
+      </div>
+    </div>
+
+    <div class="section" style="width: 70%;">
+      <form style="width: 95%;">
+        <h1>Get the Service</h1>
+        <div class="regi-head">
+          <h2>Divitional Secretariat Office</h2>
+        </div>
+
+        <div class="two-forms">
+          <div class="input-box">
+              <i class="bx bxs-user"></i>
+              <input type="text" class="input-field" placeholder="District">
+          </div>
+          <div class="input-box">
+              <i class="bx bxs-user"></i>
+              <input type="text" class="input-field" placeholder="DS Divition">
+          </div>
+        </div>
+
+        <div class="regi-head">
+          <h2>Choose Date</h2>
+        </div>
+        <img src="images/calander.png" style="margin: 30px;">
+
+        <button type="submit" class="btn">Confirm</button>
+      </form>
+    </div>
+  </div>
+
+  <?php
+  include_once 'footer.php';
+?>
+  <!--end of footer section-->
+</body>
+</html>
