908cd2b293bd8db9f28847da7a0f6f7be72057ae
Theekshana Yugan <offmailacc2022@gmail.com>
2024-06-12 13:01:11 +0530

    fix Design

---
+++ IGSR/officerServices.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Officer Service Log</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <?php include 'header.php';?>
+
+  <div class="wrapper">
+    <div class="section">
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Service</th>
+          <th>Citicen's Name</th>
+          <th>Citizen's ID no</th>
+          <th>Phone No</th>
+          <th>Venue</th>
+          <th>Status</th>
+          <th>Feedback</th>
+        </tr>
+        <tr>
+          <td>2024-05-16</td>
+          <td>Providing income cetificates</td>
+          <td>Mr. D.K.Ariyadasa</td>
+          <td>199036xxx</td>
+          <td>077xxxxxxxx</td>
+          <td> Income tax section 
+            -  
+            Embilipitiya Divisional Secretariat office</td>
+          <td>Status</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>2024-05-02</td>
+          <td>Providing income cetificates</td>
+          <td>Mr. D.K.Ariyadasa</td>
+          <td>96485xxxx</td>
+          <td>077xxxxxxxx</td>
+          <td>Income tax section 
+            -  
+            Embilipitiya Divisional Secretariat office</td>
+          <td>Audit</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>2024-04-20</td>
+          <td>Providing income cetificates</td>
+          <td>Mr. D.K.Ariyadasa</td>
+          <td>64856xxxx</td>
+          <td>077xxxxxxxx</td>
+          <td>Income tax section 
+            -  
+            Embilipitiya Divisional Secretariat office</td>
+          <td>Closed</td>
+          <td>good</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+
+
+  
+  <?php
+  include_once 'footer.php';
+?>
+  <!--end of footer section-->
+</body>
+</html>
