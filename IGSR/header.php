31a6162d99eddb5c9e959ff74395b6893871afc2
Theekshana Yugan <offmailacc2022@gmail.com>
2024-06-13 10:14:55 +0530

    Update header.php

--- IGSR/header.php
+++ IGSR/header.php
@@ -13,10 +13,22 @@
       <a href="index.php"><img src="images\logo.png" alt="logo" ></a>
     </div>
     <div class="navigation">
-          <a href="index.php">Home</a>
-          <a href="index.php#service">Services</a>
-          <a href="index.php#About">About us</a>
-          <a href="index.php#Contact">Contact</a>
+      <a href="index.php">Home</a>
+      <a class="service-btn">Services</a>
+      <div class="service-box">
+        <ul>
+          <li><a href="service1.php">Providing income certificates</a></li>
+          <li><a href="service2.php">Permits for felling wood</a></li>
+          <li><a href="service3.php">Licenses for Timber transportation</a></li>
+          <li><a href="service4.php">Permits for dumping and transportation of sand, soil, and gravel</a></li>
+          <li><a href="service5.php">Permits for dumping transportation of animals</a></li>
+          <li><a href="service6.php">Mode of obtaining Birth/Marriage/Death Certificates issued</a></li>
+          <li><a href="service7.php">Registration of Business Names</a></li>
+          <li><a href="service8.php">Registration of Voluntary Organization</a></li>
+        </ul>
+      </div>
+      <a href="/IGSR/index.php#About">About us</a>
+      <a href="/IGSR/index.php#Contact">Contact</a>
     </div>
     <div class="notific">
       <a href="#"><img src="images/bell.png"></a>
