a1b468f4d6fe4c3b40217067ef6a7138cb42f464
Theekshana Yugan <offmailacc2022@gmail.com>
2024-06-14 05:01:02 +0530

    Update officerProfile.php

--- IGSR/officerProfile.php
+++ IGSR/officerProfile.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Providing income cetificates</title>
+  <title>Officer Profile</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -11,67 +11,43 @@
   include_once 'header.php';
   ?>
 
-  <a href="officerServices.html"><button type="submit" class="btn">Service log</button></a> 
-  <div class="wrapper">
-    <div class="section" style="width: 70%;margin-top: 30px;">
-      <form style="width: 95%;">
-        <img src="images/profile.png">
-        <h1>ABC Sumanapala</h1>
-
-        <div class="profile-list">
-          <ul>
-            <li>Nic No :- 2000xxxxxxxx</li>
-            <li>Officer ID :- IGSR025xxxxxx</li>
-            <li>Phone No :- 076xxxxxxx</li>
-            <li>Position :- Secutry</li>
-            <li>Email :- abcsxxxxx@gmail.com</li>
-          </ul>
+<div class="wrapper" id="wrapper-citizenProfile">
+  <div id="service-log-buton">
+    <a href="officerServices.php" class="signbtn">Service Log</a>
+  </div>
+  <div class="section">
+    <div class="profile-details">
+      <div class="profile-section-main">
+        <img src="/images/manager.png" alt="user">
+        <h2>DK Ariyadasa</h2>
+      </div>
+      <div class="profile-section-secondary">
+        <div class="column">
+          <div class="userDetail-bar">
+            <h4>NIC No</h4><b class="divider">></b><p>2000xxxxxxxx</p>
+          </div>
+          <div class="userDetail-bar">
+            <h4>Phone No</h4><b class="divider">></b><p>076xxxxxxx</p>
+          </div><br>
+          <div class="userDetail-bar">
+            <h4>Email</h4><b class="divider">></b><p>yoxxxxxx@gmail.com</p>
+          </div>
         </div>
-
-        <button type="submit" class="btn">Sign Out</button>
-      </form>
+        <div class="column">
+          <div class="userDetail-bar">
+            <h4>officer ID</h4><b class="divider">></b><p>IGSR001xxxxx</p>
+          </div>
+          <div class="userDetail-bar">
+            <h4>Position</h4><b class="divider">></b><p>IGSR001xxxxx</p>
+          </div>
+        </div>
+      </div>
+      <div class="btn-section">
+        <a href="signin.php" class="signbtn">Sign out</a>
+      </div>
     </div>
   </div>
-  
-  <?php
-  include_once 'footer.php';
-?>
-  <!--end of footer section-->
-</body>
-</html><!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Providing income cetificates</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <?php
-  include_once 'header.php';
-  ?>
-
-  <a href="officerServices.html"><button type="submit" class="btn">Service log</button></a> 
-  <div class="wrapper">
-    <div class="section" style="width: 70%;margin-top: 30px;">
-      <form style="width: 95%;">
-        <img src="images/profile.png">
-        <h1>ABC Sumanapala</h1>
-
-        <div class="profile-list">
-          <ul>
-            <li>Nic No :- 2000xxxxxxxx</li>
-            <li>Officer ID :- IGSR025xxxxxx</li>
-            <li>Phone No :- 076xxxxxxx</li>
-            <li>Position :- Secutry</li>
-            <li>Email :- abcsxxxxx@gmail.com</li>
-          </ul>
-        </div>
-
-        <button type="submit" class="btn">Sign Out</button>
-      </form>
-    </div>
-  </div>
+</div>
   
   <?php
   include_once 'footer.php';
